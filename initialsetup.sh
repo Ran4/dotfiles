b7d35d0157f5f41969b14cd8841661e457e97608
Rasmus Ansin <rasmus.ansin@gmail.com>
2015-06-16 14:04:48 +0200

    Added openssh-server to initialsetup

--- initialsetup.sh
+++ initialsetup.sh
@@ -1,16 +1,13 @@
 sudo apt-get -y install git
+sudo apt-get -y install openssh-server
 sudo apt-get -y install vim
 sudo apt-get -y install tmux
 sudo apt-get -y install curl
 
 mkdir -p ~/pyy
-
-#get ei
 cd ~/pyy
 git clone git@github.com:Ran4/ei.git
-
 sudo cp ~/pyy/ei/ei.py /usr/lib/python2.7/
 
-#used for clip.py
-sudo apt-get -y install python-tk
+sudo apt-get -y install python-tk #used for clip.py
 
