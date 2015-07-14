00f54f628043a5fd6df5f063df9ffdee98fddcb7
Rasmus Ansin <rasmus.ansin@gmail.com>
2015-07-14 17:51:43 +0200

    Updated initialsetup to remove startup folders, fixed bug where makesymlinks ~/.dotfiles_old was instead ~/dotfiles_old, increased xterm default size

--- initialsetup.sh
+++ initialsetup.sh
@@ -4,10 +4,20 @@ sudo apt-get -y install vim-gnome
 sudo apt-get -y install tmux
 sudo apt-get -y install curl
 
+git config --global user.email "rasmus.ansin@gmail.com"
+git config --global user.name "Rasmus Ansin"
+
+#Remove annoying folders that come with ubuntu
+rmdir ~/Music
+rmdir ~/Pictures
+rmdir ~/Documents
+rmdir ~/Videos
+rmdir ~/Desktop
+
 mkdir -p ~/pyy
 cd ~/pyy
-git clone git@github.com:Ran4/ei.git
-sudo cp ~/pyy/ei/ei.py /usr/lib/python2.7/
+#git clone git@github.com:Ran4/ei.git
+#sudo cp ~/pyy/ei/ei.py /usr/lib/python2.7/
 
-sudo apt-get -y install python-tk #used for clip.py
+#sudo apt-get -y install python-tk #used for clip.py
 
