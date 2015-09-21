b3e3399a8925fde69a3af02c4fb622d40598667c
Rasmus Ansin <rasmus.ansin@gmail.com>
2015-09-21 21:24:04 +0200

    Made defaulttmuxsession.sh attach to previous session if available, fixed

--- defaulttmuxsession.sh
+++ defaulttmuxsession.sh
@@ -2,16 +2,25 @@
 # var for session name (to avoid repeated occurences)
 sn=s0
 
-# Start the session and window 0 in /etc
-#   This will also be the default cwd for new windows created
-#   via a binding unless overridden with default-path.
-cd ~
-tmux -2 attach || tmux new-session -s "$sn" -d
+function has-session {
+    tmux has-session -t $1 2>/dev/null
+}
 
-cd ~/dotfiles
-tmux new-window -n dotfiles
+has-session "s0"
+if [ "$?" -eq 1 ] ; then
+    # Start the session and window 0 in /etc
+    #   This will also be the default cwd for new windows created
+    #   via a binding unless overridden with default-path.
+    cd ~
+    tmux -2 attach || tmux new-session -s "$sn" -d
 
-# Select window #1 and attach to the session
-tmux select-window -t "$sn:1"
-tmux swap-window -t +1
-tmux -2 attach-session -t "$sn"
+    cd ~/dotfiles
+    tmux new-window -n dotfiles
+
+    # Select window #1 and attach to the session
+    tmux select-window -t "$sn:1"
+    tmux swap-window -t +1
+    tmux -2 attach-session -t "$sn"
+else
+    tmux -2 attach-session -t "$sn"
+fi
