e2429afe2b3a294a4bf0f62db1263e34d697feb2
Rasmus Ansin <rasmus.ansin@gmail.com>
2016-03-28 21:38:03 +0200

    brew_installs: Added a bunch of pygame installs, including numpy/amtplotlib/pandas. Also xquartz.

--- mac/brew_installs.sh
+++ mac/brew_installs.sh
@@ -1,4 +1,4 @@
-#Brew stuff
+brew update
 #Common linux stuff
 brew tap homebrew/dupes
 brew install findutils --with-default-names
@@ -16,9 +16,9 @@ brew install watch
 brew install git
 brew install less
 brew install openssh
-brew install python
 brew install rsync
 brew install unzip
+brew install python
 brew install vim --override-system-vi
 brew install macvim
 #brew install macvim --override-system-vim --custom-system-icons
@@ -29,6 +29,40 @@ brew install wget
 brew install htop
 brew install tree
 brew install the_silver_searcher
+
+# Python stuff
+pip install --upgrade pip
+brew install freetype
+brew install libxml2
+
+# Pygame:
+brew tap Homebrew/python
+brew update
+brew install Homebrew/python/pygame
+
+pip install nose
+brew tap homebrew/science
+#brew tap samueljohn/python
+
+brew install gcc #needed for Fortran
+brew install homebrew/python/numpy --with-openblas
+
+brew install wxpython
+
+brew link numpy
+brew install homebrew/python/matplotlib
+
+pip install pandas
+
+brew cask install xquartz
+
+
+# Things to install manually.
+# TODO: automate these things!
+#Ukulele:
+#http://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=ukelele
+
+# Possibly deprecated
 
 #In case --override-system-vi above doesn't work
 # Making OS X use brew vim instead of built-in vim
