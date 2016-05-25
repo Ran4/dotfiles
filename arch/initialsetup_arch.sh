8024bd4099b64f28bb293a05cabe199df5bb36e8
Rasmus Ansin <rasmus.ansin@gmail.com>
2016-05-25 15:30:05 +0800

    arch: Added zprofile and symlinks to it

--- arch/initialsetup_arch.sh
+++ arch/initialsetup_arch.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+dir=~/dotfiles                    # dotfiles directory
 
 function ask_yes_or_no() {
     #found at http://stackoverflow.com/questions/1885525/how-do-i-prompt-a-user-for-confirmation-in-bash-script
@@ -16,13 +17,20 @@ function ask_yes_or_no() {
     esac
 }
 
+#Help function to see if a command exists
+command_exists () {
+  type "$1" >/dev/null 2>/dev/null
+}
+
 if [[ "yes" == $(ask_yes_or_no 'Install applications via pacman?')  ]]
 then
     echo "Installing...."
     ~/dotfiles/arch/pacman_installs.sh
-else
-    echo "Not installing any applications via pacman."
 fi
 
-echo "Changing shell to zsh..."
-sudo chsh -s $(which zsh)
+# echo "Changing shell to zsh..."
+# sudo chsh -s $(which zsh)
+
+echo -e "${LBLUE}Creating symlink to ~/.zprofile (ln -s $dir/arch/zprofile ~/.zprofile)${NC}"
+ln -sf $dir/arch/zprofile ~/.zprofile
+echo "...done"
