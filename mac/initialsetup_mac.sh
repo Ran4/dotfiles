10198d6ccd379df1a3839a4ef4c217947f4d4d5d
Rasmus Ansin <rasmus.ansin@gmail.com>
2016-06-02 10:30:22 +0200

    Moved defaults.write to fix virtualbox cmd-q, cmd-h to initialsetup_mac.sh

--- mac/initialsetup_mac.sh
+++ mac/initialsetup_mac.sh
@@ -58,3 +58,6 @@ then
 else
     echo "Not doing brew and macports installs."
 fi
+
+#Replace Command-H with Command-Control-Option-H and Command-Q with Command-Control-Option-Q:
+defaults write org.virtualbox.app.VirtualBoxVM NSUserKeyEquivalents '{"Quit VirtualBox VM"="@~^q"; "Hide VirtualBox VM" = "@~^h";}'
--- mac/zshrc_mac.sh
+++
@@ -1,2 +0,0 @@
-#Replace Command-H with Command-Control-Option-H and Command-Q with Command-Control-Option-Q:
-defaults write org.virtualbox.app.VirtualBoxVM NSUserKeyEquivalents '{"Quit VirtualBox VM"="@~^q"; "Hide VirtualBox VM" = "@~^h";}'
