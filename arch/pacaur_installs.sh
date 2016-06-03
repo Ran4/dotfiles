39103e5be6bd6580459ea83614e88c39d99a646d
Rasmus Ansin <rasmus.ansin@gmail.com>
2016-06-04 05:42:52 +0800

    arch/pacman_installs.sh: removed i3, added i3-gaps to new files pacaur_installs.sh

---
+++ arch/pacaur_installs.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+pacaur -S --needed i3-gaps-git i3lock i3status
+
--- arch/pacman_installs.sh
+++ arch/pacman_installs.sh
@@ -6,12 +6,12 @@ sudo pacman -S --needed gvim #gvim has +clipboard even for the CLI version)
 sudo pacman -S --needed git
 sudo pacman -S --needed zsh tmux ack-grep the_silver_searcher tree
 sudo pacman -S --needed wget openssh xclip
-sudo pacman -S --needed python python2 python-virtualenv
+sudo pacman -S --needed python python2 python-virtualenv python-pip python2-pip
 sudo pacman -S --needed pastebinit
 sudo pacman -S --needed zathura zathura-pdf-poppler zathura-ps zathura-djvu
-sudo pacman -S --needed eog
-sudo pacman -S --needed feh
-sudo pacman -S --needed i3-wm i3lock i3status
+sudo pacman -S --needed eog feh
+sudo pacman -S --needed screenfetch
+#sudo pacman -S --needed i3-wm i3lock i3status #use i3-gaps instead
 
 ZATHRCFILE=/etc/zathurarc
 [ ! -f $ZATHRCFILE ] \
