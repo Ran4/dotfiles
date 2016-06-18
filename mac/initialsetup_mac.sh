c18b623ced8b94c16e7846cd1db461c34a235fec
Rasmus Ansin <rasmus.ansin@gmail.com>
2016-06-19 00:54:07 +0200

    mac/initialsetup_mac.sh: set expose-animation-duration to 0.05 from 0.1

--- mac/initialsetup_mac.sh
+++ mac/initialsetup_mac.sh
@@ -28,7 +28,8 @@ defaults write com.apple.dock launchanim -bool false
 defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
 
 #Speed up Mission Control animations
-defaults write com.apple.dock expose-animation-duration -float 0.1
+defaults write com.apple.dock expose-animation-duration -float 0.05
+killall Dock
 
 #Disable the delay when you hide the Dock
 defaults write com.apple.Dock autohide-delay -float 0
