16c6da61a7f45eb5c66ebc3036301107b4e67688
Rasmus Ansin <rasmus.ansin@gmail.com>
2016-08-12 04:55:16 +0800

    arch: Add xdotool and fzf to pacaur_installs.sh

--- arch/pacaur_installs.sh
+++ arch/pacaur_installs.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 pacaur -S --needed i3-gaps-git i3lock i3status
-
+pacaur -S --needed xdotool #used with ~/.i3/runorfocus.py
+pacaur -S --needed fzf
--- arch/zshrc_arch.sh
+++ arch/zshrc_arch.sh
@@ -4,5 +4,6 @@ command_exists () {
 }
 
 if command_exists feh ; then
-    feh --bg-scale ~/other/backgrounds/abstract-mosaic-background.png
+    #feh --bg-scale ~/other/backgrounds/abstract-mosaic-background.png
+    feh --bg-scale ~/other/backgrounds/wallhaven-375988_spaceship.jpg
 fi
