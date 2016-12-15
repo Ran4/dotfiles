cd38bafbb261d8b83f04efb39511e45572ccc5af
Rasmus Ansin <rasmus.ansin@gmail.com>
2016-12-15 10:41:46 +0100

    slate: add hyper-e and hyper-t binding to fullscreen/focus Terminal

--- mac/hammerspoon/init.lua
+++ mac/hammerspoon/init.lua
@@ -1,6 +1,12 @@
 require "vim"
 require "common"
 
+-- local ctrlshift = {"ctrl", "shift"}
+-- hs.hotkey.bind(ctrlshift, "up", hs.window.focusWindowNorth)
+-- hs.hotkey.bind(ctrlshift, "right", hs.window.focusWindowEast)
+-- hs.hotkey.bind(ctrlshift, "down", hs.window.focusWindowSouth)
+-- hs.hotkey.bind(ctrlshift, "q", hs.window.focusWindowWest)
+
 hs.hotkey.bind({"cmd", "ctrl"}, "R", function()
   hs.reload()
 end)
