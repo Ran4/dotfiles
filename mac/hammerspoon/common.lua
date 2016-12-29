0cdf92ad4faf5fcb8a524f7c7b82c72b8eedd951
Rasmus Ansin <rasmus.ansin@gmail.com>
2016-12-29 22:12:11 +0100

    Add inputrc. Will currently set vi mode in readline

--- mac/hammerspoon/common.lua
+++
@@ -1,4 +0,0 @@
--- ctrl+g sends ctrl
-hs.hotkey.bind({"ctrl"}, "g", function()
-    hs.eventtap.keyStroke({}, "Return")
-end)
--- mac/hammerspoon/init.lua
+++ mac/hammerspoon/init.lua
@@ -1,5 +1,4 @@
-require "vim"
-require "common"
+-- require "vim"
 
 -- local ctrlshift = {"ctrl", "shift"}
 -- hs.hotkey.bind(ctrlshift, "up", hs.window.focusWindowNorth)
@@ -7,7 +6,14 @@ require "common"
 -- hs.hotkey.bind(ctrlshift, "down", hs.window.focusWindowSouth)
 -- hs.hotkey.bind(ctrlshift, "q", hs.window.focusWindowWest)
 
+
+-- ctrl+g sends ctrl
+hs.hotkey.bind({"ctrl"}, "q", function()
+    hs.eventtap.keyStroke({}, "Return")
+end)
+
 hs.hotkey.bind({"cmd", "ctrl"}, "R", function()
   hs.reload()
 end)
+
 hs.alert.show("Config loaded")
