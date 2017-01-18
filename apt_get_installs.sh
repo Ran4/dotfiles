80fe71a972c7600b8ff8aacb635adb391a350940
Rasmus Ansin <rasmus.ansin@orexplore.com>
2017-01-18 18:10:31 +0100

    apt_get_installs: Make neovim install on one line

--- apt_get_installs.sh
+++ apt_get_installs.sh
@@ -34,12 +34,12 @@ sudo apt-get update
 sudo apt-get install git
 
 # Neovim
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt-get update
-sudo apt-get -y install neovim
-pip3 install neovim
-pip2 install neovim
+sudo apt-get install software-properties-common && \
+    sudo add-apt-repository ppa:neovim-ppa/unstable && \
+    sudo apt-get update && \
+    sudo apt-get -y install neovim && \
+    pip3 install neovim && \
+    pip2 install neovim
 
 sudo apt-get -y install python-dev python-pip python3-dev python3-pip
 #The symlinking is done in makesymlinks.sh
