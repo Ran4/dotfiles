8bd8189a49aef56c87fa66459eeaa2cadea59349
Rasmus Ansin <rasmus.ansin@gmail.com>
2017-06-21 22:57:09 +0200

    Add sshfs to apt_get_installs.sh

--- apt_get_installs.sh
+++ apt_get_installs.sh
@@ -1,6 +1,7 @@
 sudo apt-get update
 sudo apt-get -y install git
 sudo apt-get -y install openssh-server
+sudo apt-get -y install sshfs
 sudo apt-get -y install vim-gnome
 sudo apt-get -y install tmux
 sudo apt-get -y install curl
