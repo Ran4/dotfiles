34b3ad2e771f50c01e5308bcbad7d98810b3cfc6
Rasmus Ansin <rasmus.ansin@gmail.com>
2017-06-21 23:17:40 +0200

    Add rhythmbox support to nowplaying.sh and associated scripts

--- scripts/mly_nowplaying.sh
+++ scripts/mly_nowplaying.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
+command_exists () {
+    type "$1" &> /dev/null ;
+}
 
-ARTIST=$(osascript -e 'tell application "iTunes" to if player state is playing then artist of current track')
+if command_exists rhythmbox-client ; then
+    ARTIST="$(rhythmbox-client --no-start --print-playing-format %ta)"
+    SONG="$(rhythmbox-client --no-start --print-playing-format %tt)"
+else # Assume OS X, use osascript to get from iTunes instead
+    ARTIST=$(osascript -e 'tell application "iTunes" to if player state is playing then artist of current track')
+    SONG=$(osascript -e 'tell application "iTunes" to if player state is playing then name of current track')
+fi
 
-SONG=$(osascript -e 'tell application "iTunes" to if player state is playing then name of current track')
-
-#python ~/git/other/metallum/metallum.py "$(./nowplaying_artist)" "$(./nowplaying_song)"
 python ~/git/other/metallum/metallum.py "$ARTIST" "$SONG"
--- scripts/nowplaying.sh
+++ scripts/nowplaying.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-ARTIST=$(osascript -e 'tell application "iTunes" to if player state is playing then artist of current track')
+command_exists () {
+type "$1" &> /dev/null ;
+}
 
-SONG=$(osascript -e 'tell application "iTunes" to if player state is playing then name of current track')
+if command_exists rhythmbox-client ; then
+    ARTIST="$(rhythmbox-client --no-start --print-playing-format %ta)"
+    SONG="$(rhythmbox-client --no-start --print-playing-format %tt)"
+else # Assume OS X, use osascript to get from iTunes instead
+    ARTIST=$(osascript -e 'tell application "iTunes" to if player state is playing then artist of current track')
+    SONG=$(osascript -e 'tell application "iTunes" to if player state is playing then name of current track')
+fi
 
 echo $ARTIST - $SONG
