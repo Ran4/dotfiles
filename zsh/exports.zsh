864d217ad73117a45235050d0c461b5f90edc56c
Rasmus Ansin <rasmus.ansin@gmail.com>
2018-02-24 10:46:00 +0100

    Add $HOME/.yarn to fzf/exports.zsh

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,6 +1,7 @@
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
 export PATH=$PATH:/opt/local/bin
 export PATH=$PATH:~/.local/bin
+export PATH="$HOME/.yarn/bin/:$PATH"
 export PATH="$HOME/.cargo/bin:$PATH"
 [[ -f "/etc/arch-release" ]] && export PATH=$PATH:/usr/bin/core_perl/
 export MANPATH="/opt/local/share/man:/usr/local/man:$MANPATH"
