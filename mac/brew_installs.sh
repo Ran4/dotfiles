1509e4c2e3f4c41211ca1c86a05155bc105044a5
Rasmus Ansin <rasmus.ansin@gmail.com>
2018-08-02 13:35:01 +0200

    Add graphviz dot to brew_installs.sh

--- mac/brew_installs.sh
+++ mac/brew_installs.sh
@@ -43,6 +43,7 @@ brew install boost  # Needed for KarabinerElements
 brew install cloc
 brew install yarn
 brew install mono
+brew install graphviz # Graphviz dot graph tool
 
 brew install moc
 #brew services start jack #moc only works when jack is running
