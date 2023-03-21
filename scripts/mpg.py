4afd7f4abcf28e7ef1f4adf270141831c0a359a6
Rasmus Ansin <rasmus.ansin@gmail.com>
2023-03-21 19:24:26 +0100

    Add scripts/mpg.py

---
+++ scripts/mpg.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import sys
+if len(sys.argv) < 2:
+    print("Converts miles per gallon to km/100 L (both UK and US miles).")
+    print()
+    print("Usage:")
+    print("  $ mpg 34")
+    print("  30 UK mpg is 9.42 L/100 km (0.942 L/mil)")
+    exit(2)
+
+mpg = int(sys.argv[1])
+km_per_uk_gallon = mpg * 1.609
+km_per_liter = km_per_uk_gallon/4.546
+liter_per_100_km = 100 / km_per_liter
+print(
+    f"{mpg} UK mpg is {liter_per_100_km:.2f} L/100 km"
+    f" ({0.1*liter_per_100_km:.3f} L/mil)"
+)
