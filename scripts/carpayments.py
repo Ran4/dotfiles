a784d99cc0e8665d5dfa887842f64fcbf5b13c56
Rasmus Ansin <rasmus.ansin@gmail.com>
2025-12-05 17:20:59 +0100

    Add bilfinansieringskalkylator.html and update carpayments.py

--- scripts/carpayments.py
+++ scripts/carpayments.py
@@ -1,28 +1,28 @@
 for bilpris in [
-    # 722.3 + 6,
-    744.709,
-    # 630,
-    # 635,
-    500,
+    # 744.709 + 60,
+    630,
 ]:
     bilpris *= 1000
     for with_restvärde, restvärde_multiplier, lanetid_månader in [
-        (True, 0.5, 3 * 12),
-        # (True, 0.45, 4 * 12),
-        # (True, 0.3, 5 * 12),
-        # (False, 0.0, 6 * 12),
+        # (True, 0.55, 3 * 12),
+        (True, 0.25, 5 * 12),
+        # (True, 0.1, 6 * 12),
         # (False, 0.0, 4 * 12),
+        # (False, 0.0, 6 * 12),
     ]:
-        for kontantinsats in [200]:
+        for kontantinsats in [
+            175,  # + 12 * 19,
+        ]:
             kontantinsats *= 1000
 
             # bilpris = 661 * 1000
             restvarde = bilpris * restvärde_multiplier
 
-            # ranta_effektiv = (2.95 * 0.79 + 0.2) / 100
-            # ranta_effektiv = (2.95 + 0.2) / 100
-            ranta_effektiv = (5.95 * 0.79 + 0.2) / 100
-            # ranta_effektiv = (6.95 * 0.79 + 0.2) / 100
+            # ranta_effektiv = (2.95 * 0.79 + 0.05) / 100
+            # ranta_effektiv = (3.95 * 0.79 + 0.05) / 100
+            # ranta_effektiv = (4.95 * 0.79 + 0.1) / 100
+            # ranta_effektiv = (4.95 * 0.79 + 0.2) / 100
+            ranta_effektiv = (4.95 * 1.0 + 0.2) / 100
 
             manadsranta = (1 + ranta_effektiv) ** (1 / 12) - 1
             lanebelopp = bilpris - kontantinsats  # kr
